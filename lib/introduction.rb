33434486e816e07772e8fc42b22991d6dff955c9
Jorge Venegas Martinez <brangelina01@gmail.com>
2020-07-06 19:44:02 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,5 @@
-def introduction(name)
-  puts "Hi, my name is #{name}."
+def introduction(name, language)
+  puts "Hi, my name is #{name} and i am learning to program in #{language}"
 end
 
-def introduction(language)
-  puts "and i am learning to program in #{language}."
-end
+introduction(Dan, Ember)
